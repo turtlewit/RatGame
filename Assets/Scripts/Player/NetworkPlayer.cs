10a07ad75c266f7e79b9a9c2e022550306553fae
Kat Witten <turtlewit@gmail.com>
2020-02-21 17:08:07 -0800

    initial commit

---
+++ Assets/Scripts/Player/NetworkPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+[AddComponentMenu("Player/NetworkPlayer")]
+public class NetworkPlayer : NetworkBehaviour
+{
+    [Header("Objects in this list will only exist locally (e.g. the camera).")]
+    [SerializeField]
+    List<Object> LocalOnly = new List<Object>();
+
+    void Start()
+    {
+        if (!hasAuthority)
+        {
+            foreach (var obj in LocalOnly)
+                Destroy(obj);
+            
+            if (NetworkManager.singleton is NetworkManager manager)
+            {
+                if (manager.GetComponent<NetworkManagerHUD>() is NetworkManagerHUD hud)
+                {
+                    hud.enabled = false;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Player/PlayerMovement")]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = Vector3.zero;
+        direction.x = Input.GetAxis("Horizontal");
+        direction.z = Input.GetAxis("Vertical");
+        direction = direction.normalized;
+
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+}
