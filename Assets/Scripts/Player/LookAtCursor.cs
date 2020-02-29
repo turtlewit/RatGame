8d71d2174dd11f20cc0039fe589c7a1b8ee80f78
Kat Witten <turtlewit@live.com>
2020-02-28 16:00:01 -0800

    character controller progress

---
+++ Assets/Scripts/Player/LookAtCursor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCursor : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+    // Update is called once per frame
+    void Update()
+    {
+        Camera cam = Camera.main;
+        Vector3 pos = cam.WorldToScreenPoint(target.position);
+        Vector3 mouse_pos = Input.mousePosition;
+        Vector3 direction = mouse_pos - pos;
+        direction = new Vector3(direction.x, 0, direction.y).normalized;
+        target.rotation = Quaternion.LookRotation(direction, Vector3.up);
+    }
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,14 +8,20 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     float speed;
 
+    [SerializeField]
+    Rigidbody body;
+
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Vector3 direction = Vector3.zero;
         direction.x = Input.GetAxis("Horizontal");
         direction.z = Input.GetAxis("Vertical");
+        float m = direction.magnitude;
         direction = direction.normalized;
 
-        transform.Translate(direction * speed * Time.deltaTime);
+        body.velocity = direction * speed * m;
+
+        //transform.Translate(direction * speed * Time.deltaTime);
     }
 }
