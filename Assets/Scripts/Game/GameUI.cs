7515776f3aed29f42aaa422fee2fb494bdca424e
Duncan Sparks <duncan.sprks@gmail.com>
2020-03-15 17:46:22 -0700

    Start on UI

---
+++ Assets/Scripts/Game/GameUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] textObjects;
+
+    static GameUI singleton = null;
+
+
+    private GameUI()
+    {
+        if (singleton == null)
+            singleton = this;
+    }
+
+
+    static void SetPlayerScore(int player, int value)
+    {
+        singleton.textObjects[player].GetComponent<Text>().text = value.ToString();
+    }
+
+
+    static void AddPlayerScore(int player, int amount)
+    {
+        Text obj = singleton.textObjects[player].GetComponent<Text>();
+        int current = int.Parse(obj.text);
+        current += amount;
+        obj.text = current.ToString();
+    }
+}
